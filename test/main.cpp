4eefa13b64d9ce51bfa67c408c5d3b629bab6046
Felipe Guajardo <fgerman.mena@gmail.com>
2024-05-12 12:52:36 -0400

    added all the good cases for get encoder error and refactoring

--- test/main.cpp
+++ test/main.cpp
@@ -506,14 +506,23 @@ class callAndResponseCmds : public testing::TestWithParam<testParams>{
     std::thread listening_thread;
     odrive_can::OdriveCan odrv;
 
+    //params
+    GetFunction func;
+    int ans_size;
+    int n_of_good_msgs;
+    std::vector<uint64_t> good_msgs;
+
+    void param_init(){
+        func = GetParam().func;
+        ans_size = GetParam().ans_size;
+        n_of_good_msgs = GetParam().n_of_good_msgs;
+        good_msgs = GetParam().good_msgs;
+    }
     callAndResponseCmds():  odrv("vcan0",4U){} //the id has to be written as litteral otherwise the first test gets a random number
 };
 
 TEST_P(callAndResponseCmds,happy_case){
-    GetFunction func = GetParam().func;
-    int ans_size = GetParam().ans_size;
-    int n_of_good_msgs = GetParam().n_of_good_msgs;
-    std::vector<uint64_t> good_msgs = GetParam().good_msgs;
+    param_init();
 
     can_frame expected;
     expected.can_id = CAN_RTR_FLAG|(4<<5);
@@ -561,12 +570,11 @@ TEST_P(callAndResponseCmds,happy_case){
             wfmaa_thread.join();
         }
     }
-
 }
 
 INSTANTIATE_TEST_SUITE_P(Values,callAndResponseCmds,
     testing::Values(
-        testParams{GetFunction::ENCODER_ERROR,4,{0,1},2}
+        testParams{GetFunction::ENCODER_ERROR,4,{0,1,2,4,8,0x10,0x20,0x40,0x80},9}
     )
 );
 
